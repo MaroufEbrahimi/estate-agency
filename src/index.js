8af2b519edbc43e7ea230d035b80f399ab9315f4
Marouf <maroufibrahimi7264@gmail.com>
2023-03-15 12:14:54 -0700

    using React-18 and createRoot

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-render(<App />,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
 
+root.render(<App />);
