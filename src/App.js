0a265448d6c17f36c3292ee15bab03451b6a14a7
Marouf <maroufibrahimi7264@gmail.com>
2023-03-15 13:37:01 -0700

    added Footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "../src/components/Header/Header";
+import Footer from "../src/components/Footer/Footer";
 
 const App = () => {
   const [show, handleShow] = useState(false);
@@ -11,6 +12,7 @@ const App = () => {
       <Router>
         <Header show={show} handleShow={handleShow} />
         <div style={{ height: "200vh" }}></div>
+        <Footer />
       </Router>
     </div>
   );
