6cfa53bc8adab81e71018768c0ec47b4770ae729
Marouf <maroufibrahimi7264@gmail.com>
2023-03-14 20:04:21 -0700

    added links to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,41 @@
 import React from "react";
 
+import "./Header.css";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header_logo">
+        <a href="/">
+          Estate<span>Agency</span>
+        </a>
+      </div>
+
+      <div className="nav_links">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/property">Property</a>
+          </li>
+          <li>
+            <a href="/blog">Blog</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="header_search_icon">
+        <i>a</i>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
