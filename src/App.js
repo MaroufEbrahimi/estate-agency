3e2fc2c62fb97ad02f88928a7e2cb2c0a674971e
Marouf <maroufibrahimi7264@gmail.com>
2023-03-16 14:30:27 -0700

    added Layout component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "../src/components/Header/Header";
 import Footer from "../src/components/Footer/Footer";
+import Layout from "../src/components/Layout/Layout";
 
 const App = () => {
   const [show, handleShow] = useState(false);
@@ -11,7 +12,9 @@ const App = () => {
     <div className="app">
       <Router>
         <Header show={show} handleShow={handleShow} />
-        <div style={{ height: "200vh" }}></div>
+        <Layout>
+          <div style={{ height: "200vh" }}></div>
+        </Layout>
         <Footer />
       </Router>
     </div>
