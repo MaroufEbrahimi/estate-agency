e4ae8febeec5cf0728b52c751d575179b96a495e
Marouf <maroufibrahimi7264@gmail.com>
2023-03-16 13:01:13 -0700

    added a class for links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,30 +7,41 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer_container">
-        <div className="footer_private_links">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/property">Property</Link>
-          <Link to="/blog">Blog</Link>
-          <Link to="/contact">Contact</Link>
+        <div className="footer_links">
+          <div className="footer_private_links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/property">Property</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+
+          <div className="footer_social_links">
+            <Link to="/">
+              <i className="fa fa-twitter"></i>
+            </Link>
+            <Link to="/">
+              <i className="fa fa-instagram"></i>
+            </Link>
+            <Link to="/">
+              <i className="fa fa-linkedin"></i>
+            </Link>
+            <Link to="/">
+              <i className="fa fa-github"></i>
+            </Link>
+            <Link to="/">
+              <i className="fa fa-pinterest"></i>
+            </Link>
+          </div>
         </div>
 
-        <div className="footer_social_links">
-          <Link to="/">
-            <i className="fa fa-twitter"></i>
-          </Link>
-          <Link to="/">
-            <i className="fa fa-instagram"></i>
-          </Link>
-          <Link to="/">
-            <i className="fa fa-linkedin"></i>
-          </Link>
-          <Link to="/">
-            <i className="fa fa-github"></i>
-          </Link>
-          <Link to="/">
-            <i className="fa fa-pinterest"></i>
-          </Link>
+        <div className="footer_copyright">
+          <p>
+            © copyright <span>EstateAgency</span> all rights reserved
+          </p>
+          <p>
+            Designed by <Link to="">MaroufEbrahimi</Link>
+          </p>
         </div>
       </div>
     </div>
