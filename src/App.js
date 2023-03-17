5d398896b916785b032a029b6b0ff1af2611a3f5
Marouf <maroufibrahimi7264@gmail.com>
2023-03-17 13:00:02 -0700

    return Modal and created state for this

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "../src/components/Header/Header";
 import Footer from "../src/components/Footer/Footer";
 import Layout from "../src/components/Layout/Layout";
+import Modal from "./components/Modal/Modal";
 
-const App = () => {
+const App = (props) => {
   const [show, handleShow] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const showModalHandler = () => {
+    setShowModal(true);
+  };
+
+  const modalCloseHandler = () => {
+    setShowModal(false);
+  };
 
   return (
     <div className="app">
+      <Modal showBackdrop={showModal} removeModal={modalCloseHandler}></Modal>
       <Router>
-        <Header show={show} handleShow={handleShow} />
+        <Header
+          show={show}
+          handleShow={handleShow}
+          headerSearch={showModalHandler}
+        />
         <Layout>
           <div style={{ height: "200vh" }}></div>
         </Layout>
