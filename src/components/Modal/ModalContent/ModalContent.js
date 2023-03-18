02fc2f49a9f99bf56c5f77170336231dc16e392b
Marouf <maroufibrahimi7264@gmail.com>
2023-03-18 11:56:03 -0700

    added search title

--- src/components/Modal/ModalContent/ModalContent.js
+++ src/components/Modal/ModalContent/ModalContent.js
@@ -2,8 +2,19 @@ import React from "react";
 
 import "./ModalContent.css";
 
-const ModalContent = () => {
-  return <div>ModalContent</div>;
+const ModalContent = (props) => {
+  return (
+    <div className="search_property">
+      <div className="search_property_title">
+        <h1>
+          <span>sear</span>ch property
+        </h1>
+        <button className="search_property_close" onClick={props.removeModal}>
+          &times;
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ModalContent;
