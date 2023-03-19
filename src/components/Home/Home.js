a30c215e8bd40dc8a95d7ea57db9d0df146307c2
Marouf <maroufibrahimi7264@gmail.com>
2023-03-19 13:19:25 -0700

    style for our services

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <div className="our_services">
+        <h1>our services</h1>
+        <div className="our_services_columns">
+          <div className="our_services_col">
+            <div className="our_services_title">
+              <span className="fa fa-gamepad">2</span>
+              <h2>Lifestyle</h2>
+            </div>
+            <p>
+              Sed porttitor lectus nibh. Cras ultricies ligula sed magna dictum
+              porta. Praesent sapien massa, convallis a pellentesque nec,
+              egestas non nisi.
+            </p>
+            <a href="#">
+              <span>
+                Read more <i className="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
 
-export default Home
+          <div className="our_services_col">
+            <div className="our_services_title">
+              <span className="fa fa-usd">2</span>
+              <h2>Loans</h2>
+            </div>
+            <p>
+              Nulla porttitor accumsan tincidunt. Curabitur aliquet quam id dui
+              posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh
+              pulvinar a.
+            </p>
+            <a href="#">
+              <span>
+                Read more <i className="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+
+          <div className="our_services_col">
+            <div className="our_services_title">
+              <span className="fa fa-home">2</span>
+              <h2>Sell</h2>
+            </div>
+            <p>
+              Sed porttitor lectus nibh. Cras ultricies ligula sed magna dictum
+              porta. Praesent sapien massa, convallis a pellentesque nec,
+              egestas non nisi.
+            </p>
+            <a href="#">
+              <span>
+                Read more <i className="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
