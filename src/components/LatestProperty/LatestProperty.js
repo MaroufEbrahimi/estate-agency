db26162e4d2c2f61060ab429b57d59bce558af39
Marouf <maroufibrahimi7264@gmail.com>
2023-03-19 15:36:28 -0700

    added latest property details

--- src/components/LatestProperty/LatestProperty.js
+++ src/components/LatestProperty/LatestProperty.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const LatestProperty = () => {
+import "./LatestProperty.css";
+
+const LatestProperty = (props) => {
   return (
-    <div>LatestProperty</div>
-  )
-}
+    <div className="latest_property_details">
+      <img src={props.img} />
+      <div className="property_img_layer">
+        <div className="property_img_details">
+          <h3>{props.title}</h3>
+          <p>{props.rent}</p>
+          <Link to="/">{props.view}</Link>
+          <i className="fa fa-angle-right"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LatestProperty
+export default LatestProperty;
