ab3d461d6d10e1184b05bda65df1df6b3d3225f2
Marouf <maroufibrahimi7264@gmail.com>
2023-03-20 15:52:59 -0700

    added layer agent

--- src/components/Agent/Agent.js
+++ src/components/Agent/Agent.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './Agent.css'
+import "./Agent.css";
 
 const Agent = (props) => {
   return (
-     <div className='agent'>
-        <div className='agent_img'>
-           <img src={props.img} />
+    <div className="agent">
+      <div className="agent_img">
+        <img src={props.img} />
+      </div>
+      <div className="agent_layer">
+        <div className="agent_layer_details">
+          <Link to="/" className="layer_header_name">
+            {props.name}
+          </Link>
+          <div className="agent_layer_text">
+            <p>{props.text}</p>
+          </div>
+          <div className="agent_layer_network">
+            <p>
+              <span>Phone:</span> {props.phone}
+            </p>
+            <p>
+              <span>Email:</span> {props.email}
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Agent
+export default Agent;
