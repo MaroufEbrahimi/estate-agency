a0975db17081919690d0316b6758f98d9188db12
Marouf <maroufibrahimi7264@gmail.com>
2023-03-21 20:30:22 -0700

    testimonials details

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 import "./Testimonials.css";
 
-const Testimonials = () => {
+const Testimonials = (props) => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <div className="testimonials_details">
+      <div className="testimonials_details_box">
+        <img src={props.img} />
+      </div>
+      <div className="testimonials_details_box">
+        <span class="ion-ios-quote"></span>
+        <p>{props.text}</p>
+        <div>
+          <img src={props.miniImg} className="testimonials_name_img" />
+          <p>{props.name}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
