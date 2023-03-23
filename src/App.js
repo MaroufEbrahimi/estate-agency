e22c03f09b78e4baefb6aeb36d545f04246d8150
Marouf <maroufibrahimi7264@gmail.com>
2023-03-23 16:12:03 -0700

    added event for BackToTop com

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "../src/components/Header/Header";
@@ -6,11 +6,24 @@ import Footer from "../src/components/Footer/Footer";
 import Layout from "../src/components/Layout/Layout";
 import Modal from "./components/Modal/Modal";
 import ModalContent from "./components/Modal/ModalContent/ModalContent";
-import Home from './components/Home/Home'
+import Home from "./components/Home/Home";
+import BackToTop from "./components/BackToTop/BackToTop";
 
 const App = (props) => {
   const [show, handleShow] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [backToTop, setBackToTop] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 90) {
+        setBackToTop(true);
+      } else setBackToTop(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
 
   const showModalHandler = () => {
     setShowModal(true);
@@ -37,6 +50,9 @@ const App = (props) => {
         </Layout>
         <Footer />
       </Router>
+      <div className="back_to_top">
+        <BackToTop backToTop={backToTop} setBackToTop={setBackToTop} />
+      </div>
     </div>
   );
 };
