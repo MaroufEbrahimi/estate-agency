32db8ba49668989075a5b8b934d01650db6ff9d1
Marouf <maroufibrahimi7264@gmail.com>
2023-03-24 15:15:34 -0700

    added Agent component on About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
 
-import './About.css'
+import Agent from "../Agent/Agent";
+
+import "./About.css";
+
+import agent7 from "../../assets/agents/agent-7.jpg";
+import agent6 from "../../assets/agents/agent-6.jpg";
+import agent5 from "../../assets/agents/agent-5.jpg";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about">
+      <div className="best_agents_boxes">
+        <div className="best_agents_box">
+          <Agent
+            img={agent7}
+            name="Ahmad Ali Ahmadi"
+            text="Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two."
+            phone="+93 123 1456 11"
+            email="info@exp.com"
+          />
+        </div>
+        <div className="best_agents_box">
+          <Agent
+            img={agent6}
+            name="Moahmmad Nabi Moahmmadi"
+            text="Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two."
+            phone="+93 123 1456 12"
+            email="info@exp.com"
+          />
+        </div>
+        <div className="best_agents_box">
+          <Agent
+            img={agent5}
+            name="Marouf Famous Ebrahimi"
+            text="Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two."
+            phone="+93 123 1456 13"
+            email="info@exp.com"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
