0c42aa15c8e6188518f92fbdaa2297d54da6db7c
Marouf <maroufibrahimi7264@gmail.com>
2023-03-24 15:53:11 -0700

    added about title

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Agent from "../Agent/Agent";
 
@@ -11,33 +12,50 @@ import agent5 from "../../assets/agents/agent-5.jpg";
 const About = () => {
   return (
     <div className="about">
-      <div className="best_agents_boxes">
-        <div className="best_agents_box">
-          <Agent
-            img={agent7}
-            name="Ahmad Ali Ahmadi"
-            text="Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two."
-            phone="+93 123 1456 11"
-            email="info@exp.com"
-          />
+      <div className="section_1">
+        <div className="about_title">
+          <h1>We Do Great Design For Creative Folks</h1>
+          <p>
+            <Link to="/">
+              <span>Home </span>
+            </Link>
+            / About
+          </p>
         </div>
-        <div className="best_agents_box">
-          <Agent
-            img={agent6}
-            name="Moahmmad Nabi Moahmmadi"
-            text="Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two."
-            phone="+93 123 1456 12"
-            email="info@exp.com"
-          />
+      </div>
+
+      <div className="meet_our_team">
+        <div className="best_agents_title">
+          <h1>Meet Our Team</h1>
         </div>
-        <div className="best_agents_box">
-          <Agent
-            img={agent5}
-            name="Marouf Famous Ebrahimi"
-            text="Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two."
-            phone="+93 123 1456 13"
-            email="info@exp.com"
-          />
+        <div className="best_agents_boxes">
+          <div className="best_agents_box">
+            <Agent
+              img={agent7}
+              name="Ahmad Ali Ahmadi"
+              text="Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two."
+              phone="+93 123 1456 11"
+              email="info@exp.com"
+            />
+          </div>
+          <div className="best_agents_box">
+            <Agent
+              img={agent6}
+              name="Moahmmad Nabi Moahmmadi"
+              text="Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two."
+              phone="+93 123 1456 12"
+              email="info@exp.com"
+            />
+          </div>
+          <div className="best_agents_box">
+            <Agent
+              img={agent5}
+              name="Marouf Famous Ebrahimi"
+              text="Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two."
+              phone="+93 123 1456 13"
+              email="info@exp.com"
+            />
+          </div>
         </div>
       </div>
     </div>
