a12bf16911a89c4d9b0c61db67776a1c3ad1e866
Marouf <maroufibrahimi7264@gmail.com>
2023-03-26 13:02:54 -0700

    created Blog component

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,22 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import "./Blog.css";
 
-const Blog = (props) => {
-  return (
-    <div className="blog">
-      <img src={props.img} />
-
-      <div className="blog_detail">
-        <div className="blog_details">
-          <Link to="/">{props.link}</Link>
-          <h3>{props.text}</h3>
-          <p>{props.date}</p>
-        </div>
-      </div>
-    </div>
-  );
+const Blog = () => {
+  return <div>Blog</div>;
 };
 
 export default Blog;
