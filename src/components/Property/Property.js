84d345d355faafb9c311f804e9d0f6272a6ff341
Marouf <maroufibrahimi7264@gmail.com>
2023-03-25 21:58:58 -0700

    added LatestPropterty component to Property

--- src/components/Property/Property.js
+++ src/components/Property/Property.js
@@ -1,7 +1,17 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 import "./Property.css";
+
+import LatestProperty from "../LatestProperty/LatestProperty";
+
+// images
+import property_4 from "../../assets/latest-property/property-4.jpg";
+import property_5 from "../../assets/latest-property/property-5.jpg";
+import property_6 from "../../assets/latest-property/property-6.jpg";
+import property_7 from "../../assets/latest-property/property-7.jpg";
+import property_8 from "../../assets/latest-property/property-8.jpg";
+import property_9 from "../../assets/latest-property/property-9.jpg";
 
 const Property = () => {
   return (
@@ -17,6 +27,57 @@ const Property = () => {
           </Link>
           / Properties Grid
         </p>
+      </div>
+
+      <div className="latest_property_boxes">
+        <div className="latest_property_box">
+          <LatestProperty
+            img={property_4}
+            title="206 Mount Olive Road Two"
+            rent="RENT | $ 12.000"
+            view="Click here to view"
+          />
+        </div>
+        <div className="latest_property_box">
+          <LatestProperty
+            img={property_5}
+            title="157 West Central Park"
+            rent="RENT | $ 12.000"
+            view="Click here to view"
+          />
+        </div>
+        <div className="latest_property_box">
+          <LatestProperty
+            img={property_6}
+            title="245 Azabu Nishi Park Let"
+            rent="RENT | $ 12.000"
+            view="Click here to view"
+          />
+        </div>
+        <div className="latest_property_box">
+          <LatestProperty
+            img={property_7}
+            title="245 Azabu Nishi Park Let"
+            rent="RENT | $ 12.000"
+            view="Click here to view"
+          />
+        </div>
+        <div className="latest_property_box">
+          <LatestProperty
+            img={property_8}
+            title="245 Azabu Nishi Park Let"
+            rent="RENT | $ 12.000"
+            view="Click here to view"
+          />
+        </div>
+        <div className="latest_property_box">
+          <LatestProperty
+            img={property_9}
+            title="245 Azabu Nishi Park Let"
+            rent="RENT | $ 12.000"
+            view="Click here to view"
+          />
+        </div>
       </div>
     </div>
   );
