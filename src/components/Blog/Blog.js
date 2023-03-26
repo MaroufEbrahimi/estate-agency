ab566ede7b6e9db58778fc0661f83929ad5d208f
Marouf <maroufibrahimi7264@gmail.com>
2023-03-26 13:25:49 -0700

    added Blog title

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Blog.css";
 
 const Blog = () => {
-  return <div className="blog"></div>;
+  return (
+    <div className="blog">
+      <div className="blog_title">
+        <div>
+          <h1>Our Amazing Posts</h1>
+          <p>Grid News</p>
+        </div>
+        <p>
+          <Link to="/">
+            <span>Home </span>
+          </Link>
+          / News Grid
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
