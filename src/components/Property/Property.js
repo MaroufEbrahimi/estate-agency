c8fb68a6f84461bbd282a83af69cac131f565892
Marouf <maroufibrahimi7264@gmail.com>
2023-03-25 21:50:07 -0700

    added property title

--- src/components/Property/Property.js
+++ src/components/Property/Property.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import {Link} from 'react-router-dom'
 
 import "./Property.css";
 
 const Property = () => {
   return (
-    <div>Property</div>
-  )
-}
+    <div className="property">
+      <div className="property_title">
+        <div>
+          <h1>Our Amazing Properties</h1>
+          <p>Grid Properties</p>
+        </div>
+        <p>
+          <Link to="/">
+            <span>Home </span>
+          </Link>
+          / Properties Grid
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Property
+export default Property;
