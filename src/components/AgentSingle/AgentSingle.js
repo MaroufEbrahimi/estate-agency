b4213c1e69edec5b031f795c0ca1e5d7445de796
Marouf <maroufibrahimi7264@gmail.com>
2023-03-28 13:06:27 -0700

    created AgentSingle component

---
+++ src/components/AgentSingle/AgentSingle.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./AgentSingle.css";
+
+const AgentSingle = () => {
+  return <div className="agent_single">AgentSingle</div>;
+};
+
+export default AgentSingle;
