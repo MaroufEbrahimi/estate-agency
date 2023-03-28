121918bc309d2950166e7fff3d33c8cb00dfc64e
Marouf <maroufibrahimi7264@gmail.com>
2023-03-28 09:30:54 -0700

    added form for Contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -24,6 +24,36 @@ const Contact = () => {
           / Contact
         </p>
       </div>
+
+      <div className="contact_section2">
+        <div className="contact_section2_boxes">
+          <form>
+            <div className="contact_search_box">
+              <input type="text" placeholder="Your Name" />
+            </div>
+
+            <div className="contact_search_box">
+              <input type="text" placeholder="Your Email" />
+            </div>
+
+            <div className="contact_search_box full_box">
+              <input type="text" placeholder="Subject" />
+            </div>
+
+            <div className="contact_search_box full_box">
+              <textarea cols="30" rows="8" placeholder="Message"></textarea>
+            </div>
+
+            <div className="contact_search_box">
+              <button className="btn" type="submit">
+                Send Message
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <div className="contact_section2_box"></div>
+      </div>
     </div>
   );
 };
