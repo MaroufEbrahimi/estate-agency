3015eec76b204a35b4772df9e8a92382ff585a4e
Marouf <maroufibrahimi7264@gmail.com>
2023-03-28 13:14:59 -0700

    added a title for agent

--- src/components/AgentSingle/AgentSingle.js
+++ src/components/AgentSingle/AgentSingle.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./AgentSingle.css";
 
 const AgentSingle = () => {
-  return <div className="agent_single">AgentSingle</div>;
+  return (
+    <div className="agent_single">
+      <div className="agent_single_title">
+        <div>
+          <h1>Margaret Stone</h1>
+          <p>Agent Immobiliari</p>
+        </div>
+        <p>
+          <Link to="/">
+            <span>Home </span>
+          </Link>
+          /
+          <Link to="/">
+            <span> Agents </span>
+          </Link>
+          / Margaret Stone
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default AgentSingle;
