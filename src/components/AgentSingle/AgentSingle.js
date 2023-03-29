7a92a937479988bbc90f5468fc6d359574be4e7e
Marouf <maroufibrahimi7264@gmail.com>
2023-03-29 13:36:35 -0700

    added another section for AgentSingle

--- src/components/AgentSingle/AgentSingle.js
+++ src/components/AgentSingle/AgentSingle.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 
 import "./AgentSingle.css";
 
+// images
+import agent7 from "../../assets/agents/agent-7.jpg";
+
 const AgentSingle = () => {
   return (
     <div className="agent_single">
@@ -21,6 +24,63 @@ const AgentSingle = () => {
           </Link>
           / Margaret Stone
         </p>
+      </div>
+
+      <div className="agent_single_section1">
+        <div className="agent_single_box">
+          <img src={agent7} />
+        </div>
+        <div className="agent_single_box">
+          <h3>
+            Margaret Stone
+            <br />
+            Escala
+          </h3>
+          <p>
+            Sed porttitor lectus nibh. Praesent sapien massa, convallis a
+            pellentesque nec, egestas non nisi. Vivamus suscipit tortor eget
+            felis porttitor volutpat. Vivamus suscipit tortor eget felis
+            porttitor volutpat.
+          </p>
+          <p>
+            <span>Phone: </span>
+            +93 123 123456
+          </p>
+          <p>
+            <span>Mobile: </span>
+            1234 4321 5678
+          </p>
+          <p>
+            <span>Email: </span>
+            famous@fm.com
+          </p>
+          <p>
+            <span>Skype: </span>
+            famousFM
+          </p>
+          <p>
+            <span>Linkedin: </span>
+            famous fm
+          </p>
+
+          <div className="agent_single_social_links">
+            <Link to="/">
+              <i className="fa fa-twitter"></i>
+            </Link>
+            <Link to="/">
+              <i className="fa fa-instagram"></i>
+            </Link>
+            <Link to="/">
+              <i className="fa fa-linkedin"></i>
+            </Link>
+            <Link to="/">
+              <i className="fa fa-github"></i>
+            </Link>
+            <Link to="/">
+              <i className="fa fa-pinterest"></i>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
