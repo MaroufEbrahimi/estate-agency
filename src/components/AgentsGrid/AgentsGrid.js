7bc805fa5b9ed1f99dc31d23453ae0d42849d9bf
Marouf <maroufibrahimi7264@gmail.com>
2023-03-29 18:05:21 -0700

    added AgentsGrid title

--- src/components/AgentsGrid/AgentsGrid.js
+++ src/components/AgentsGrid/AgentsGrid.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 import "./AgentsGrid.css";
 
 const AgentsGrid = () => {
   return (
-    <div className='agent_grid'>AgentsGrid</div>
-  )
-}
+    <div className="agent_grid">
+      <div className="agent_grid_title">
+        <div>
+          <h1>Our Amazing Agents</h1>
+          <p>Grid Properties</p>
+        </div>
+        <p>
+          <Link to="/">
+            <span>Home </span>
+          </Link>
+          / Agents Grid
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default AgentsGrid
+export default AgentsGrid;
