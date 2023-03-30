cc70f2279f8d486e2f136f457035bbeff70a6623
Marouf <maroufibrahimi7264@gmail.com>
2023-03-29 18:01:26 -0700

    created AgentsGrid component

---
+++ src/components/AgentsGrid/AgentsGrid.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./AgentsGrid.css";
+
+const AgentsGrid = () => {
+  return (
+    <div className='agent_grid'>AgentsGrid</div>
+  )
+}
+
+export default AgentsGrid
