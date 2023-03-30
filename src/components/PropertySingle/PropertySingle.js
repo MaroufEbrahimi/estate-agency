54e406396473d6997b94225616586e6e4ca64401
Marouf <maroufibrahimi7264@gmail.com>
2023-03-30 15:02:47 -0700

    created Property single com

---
+++ src/components/PropertySingle/PropertySingle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./PropertySingle.css";
+
+const PropertySingle = () => {
+  return (
+    <div className='property_single'>PropertySingle</div>
+  )
+}
+
+export default PropertySingle
