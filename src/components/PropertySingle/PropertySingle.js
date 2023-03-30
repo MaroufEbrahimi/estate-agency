0093cd1d626145d7dd8dad9b8f424987efe77627
Marouf <maroufibrahimi7264@gmail.com>
2023-03-30 16:54:27 -0700

    added agent for contact

--- src/components/PropertySingle/PropertySingle.js
+++ src/components/PropertySingle/PropertySingle.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 
 import "./PropertySingle.css";
 
+// images
+import agent4 from "../../assets/agents/agent-4.jpg";
+
 const PropertySingle = () => {
   return (
     <div className="property_single">
@@ -21,6 +24,41 @@ const PropertySingle = () => {
           </Link>
           / 304 Blaster Up
         </p>
+      </div>
+
+      <div className="property_single_section5">
+        <h1>Contact Agent</h1>
+        <div className="property_single_section5_boxes">
+          <div className="property_single_section5_box property_single_section5_box1">
+            <div className="section5_box">
+              <img src={agent4} />
+            </div>
+
+            <div className="section5_box">
+              <h3>Anabella Geller</h3>
+              <p>
+                Nulla porttitor accumsan tincidunt. Vestibulum ac diam sit amet
+                quam vehicula elementum sed sit amet dui. Quisque velit nisi,
+                pretium ut lacinia in, elementum id enim.
+              </p>
+            </div>
+          </div>
+
+          <div className="property_single_section5_box property_single_section5_box2">
+            <div>
+              <input type="text" placeholder="Name *" />
+            </div>
+            <div>
+              <input type="text" placeholder="Email *" />
+            </div>
+            <div>
+              <textarea cols="30" rows="8" placeholder="Comment *"></textarea>
+            </div>
+            <div>
+              <button className="btn">Send Message</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
