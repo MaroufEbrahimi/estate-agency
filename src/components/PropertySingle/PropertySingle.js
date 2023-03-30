94b164b101327e699a393ee7ec7eb17b2285d9a7
Marouf <maroufibrahimi7264@gmail.com>
2023-03-30 15:07:19 -0700

    added title for property single

--- src/components/PropertySingle/PropertySingle.js
+++ src/components/PropertySingle/PropertySingle.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 import "./PropertySingle.css";
 
 const PropertySingle = () => {
   return (
-    <div className='property_single'>PropertySingle</div>
-  )
-}
+    <div className="property_single">
+      <div className="property_single_title">
+        <div>
+          <h1>304 Blaster Up</h1>
+          <p>Chicago, IL 606543</p>
+        </div>
+        <p>
+          <Link to="/">
+            <span>Home </span>
+          </Link>
+          /
+          <Link to="/">
+            <span> Properties </span>
+          </Link>
+          / 304 Blaster Up
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default PropertySingle
+export default PropertySingle;
