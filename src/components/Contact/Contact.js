46db17499bc1911c7f2ea33b9f58278f07f7a2b8
Marouf <maroufibrahimi7264@gmail.com>
2023-04-01 16:21:27 -0700

    added iframe to Contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -25,6 +25,15 @@ const Contact = () => {
         </p>
       </div>
 
+      <div className="contact_section1">
+        <div id="map" className="contact-map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.1422937950147!2d-73.98731968482413!3d40.75889497932681!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25855c6480299%3A0x55194ec5a1ae072e!2sTimes+Square!5e0!3m2!1ses-419!2sve!4v1510329142834"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+
       <div className="contact_section2">
         <div className="contact_section2_boxes">
           <form>
