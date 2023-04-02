c619831d1a51503bc86eafbb47c5ea1df54a7987
Marouf <maroufibrahimi7264@gmail.com>
2023-04-01 19:36:22 -0700

    added a Indicator for change slide

--- src/components/PropertySingle/Slider/Slider.js
+++ src/components/PropertySingle/Slider/Slider.js
@@ -2,8 +2,32 @@ import React, { useState } from "react";
 
 import "./Slider.css";
 
+const Indicator = ({ currenSlide, amountSlides, nextSlide }) => {
+  return (
+    <div className="indicatorWrapper">
+      {Array(amountSlides)
+        .fill(1)
+        .map((_, i) => (
+          <div
+            className="dot"
+            key={i}
+            style={{
+              background: currenSlide === i ? `#ee82ee` : `transparent`,
+            }}
+            onClick={() => nextSlide(i)}
+          ></div>
+        ))}
+    </div>
+  );
+};
+
 const Slider = ({ images = [], ...props }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = (slideIndex = currentSlide + 1) => {
+    const currentSlideIndex = slideIndex >= images.length ? 0 : slideIndex;
+    setCurrentSlide(currentSlideIndex);
+  };
 
   return (
     <div className="image_slider" {...props}>
@@ -13,10 +37,15 @@ const Slider = ({ images = [], ...props }) => {
           key={index}
           style={{
             backgroundImage: `url(${item})`,
-            marginLeft: index === 0 ? `-${currentSlide * 100}%` : null,
+            marginLeft: index === 0 ? `-${currentSlide * 124}%` : null,
           }}
         ></div>
       ))}
+      <Indicator
+        currenSlide={currentSlide}
+        amountSlides={images.length}
+        nextSlide={nextSlide}
+      />
     </div>
   );
 };
