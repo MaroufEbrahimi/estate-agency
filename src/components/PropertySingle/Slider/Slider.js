2f538cda58d96e49887a5f6e12fb32e4825ebf9e
Marouf <maroufibrahimi7264@gmail.com>
2023-04-01 19:28:06 -0700

    maped every image

--- src/components/PropertySingle/Slider/Slider.js
+++ src/components/PropertySingle/Slider/Slider.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import './Slider.css'
+import "./Slider.css";
 
-const Slider = () => {
+const Slider = ({ images = [], ...props }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   return (
-    <div>Slider</div>
-  )
-}
+    <div className="image_slider" {...props}>
+      {images.map((item, index) => (
+        <div
+          className="slide"
+          key={index}
+          style={{
+            backgroundImage: `url(${item})`,
+            marginLeft: index === 0 ? `-${currentSlide * 100}%` : null,
+          }}
+        ></div>
+      ))}
+    </div>
+  );
+};
 
-export default Slider
+export default Slider;
