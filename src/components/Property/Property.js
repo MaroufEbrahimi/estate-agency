720a26d9b5692e90fc76ef36d229d9f1c222e12c
Marouf <maroufibrahimi7264@gmail.com>
2023-04-02 20:20:00 -0700

    removed img

--- src/components/Property/Property.js
+++ src/components/Property/Property.js
@@ -5,15 +5,14 @@ import "./Property.css";
 
 import LatestProperty from "../LatestProperty/LatestProperty";
 
-// images
-import property_4 from "../../assets/latest-property/property-4.jpg";
-import property_5 from "../../assets/latest-property/property-5.jpg";
-import property_6 from "../../assets/latest-property/property-6.jpg";
-import property_7 from "../../assets/latest-property/property-7.jpg";
-import property_8 from "../../assets/latest-property/property-8.jpg";
-import property_9 from "../../assets/latest-property/property-9.jpg";
-
-const Property = () => {
+const Property = ({
+  property_4,
+  property_5,
+  property_6,
+  property_7,
+  property_8,
+  property_9,
+}) => {
   return (
     <div className="property">
       <div className="property_title">
