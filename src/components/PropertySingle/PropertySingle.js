11c252bfaaa42ab42320501fb9479527dc4bccc5
Marouf <maroufibrahimi7264@gmail.com>
2023-04-03 21:31:43 -0700

    added property description

--- src/components/PropertySingle/PropertySingle.js
+++ src/components/PropertySingle/PropertySingle.js
@@ -37,6 +37,96 @@ const PropertySingle = (props) => {
         />
       </div>
 
+      <div className="property_single_section2">
+        <div className="property_single_section2_boxes">
+          <div className="property_single_section2_box">
+            <div className="card_title_money">
+              <div className="title_money_icon">
+                <span>$</span>
+              </div>
+              <div className="title_money_quantity">
+                <h5>15000</h5>
+              </div>
+            </div>
+            <div className="property_summery">
+              <h3>Quick Summary</h3>
+              <ul>
+                <li>
+                  <strong>Property ID:</strong>
+                  <span>1134</span>
+                </li>
+                <li>
+                  <strong>Location:</strong>
+                  <span>Chicago, IL 606543</span>
+                </li>
+                <li>
+                  <strong>Property Type:</strong>
+                  <span>House</span>
+                </li>
+                <li>
+                  <strong>Status:</strong>
+                  <span>Sale</span>
+                </li>
+                <li>
+                  <strong>Area:</strong>
+                  <span>
+                    340m
+                    <sup>2</sup>
+                  </span>
+                </li>
+                <li>
+                  <strong>Beds:</strong>
+                  <span>4</span>
+                </li>
+                <li>
+                  <strong>Baths:</strong>
+                  <span>2</span>
+                </li>
+                <li>
+                  <strong>Garage:</strong>
+                  <span>1</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="property_single_section2_box">
+            <div className="property_description">
+              <h3>Property Description</h3>
+              <p>
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia Curae; Donec velit neque, auctor sit amet
+                aliquam vel, ullamcorper sit amet ligula. Cras ultricies ligula
+                sed magna dictum porta. Curabitur aliquet quam id dui posuere
+                blandit. Mauris blandit aliquet elit, eget tincidunt nibh
+                pulvinar quam id dui posuere blandit.
+              </p>
+              <p>
+                Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+                Donec rutrum congue leo eget malesuada. Quisque velit nisi,
+                pretium ut lacinia in, elementum id enim. Donec sollicitudin
+                molestie malesuada.
+              </p>
+            </div>
+
+            <div className="propery_amenity">
+              <h3>Amenities</h3>
+              <ul>
+                <li>Balcony</li>
+                <li>Outdoor Kitchen</li>
+                <li>Cable Tv</li>
+                <li>Deck</li>
+                <li>Tennis Courts</li>
+                <li>Internet</li>
+                <li>Parking</li>
+                <li>Sun Room</li>
+                <li>Concrete Flooring</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="property_single_section5">
         <h1>Contact Agent</h1>
         <div className="property_single_section5_boxes">
