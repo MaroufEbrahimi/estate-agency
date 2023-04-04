e19899fa2e5906d8b95b97baf31186749503d690
Marouf <maroufibrahimi7264@gmail.com>
2023-04-03 22:02:07 -0700

    added title and section one for BlogDetails

--- src/components/BlogDetails/BlogDetails.js
+++ src/components/BlogDetails/BlogDetails.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './BlogDetails.css'
+import "./BlogDetails.css";
 
-const BlogDetails = () => {
+const BlogDetails = ({ propertySlide3 }) => {
   return (
-    <div className='blog_details'>BlogDetails</div>
-  )
-}
+    <div className="blog_details">
+      <div className="blog_details_title">
+        <div>
+          <h1>Book Cover Deisgn</h1>
+          <p>News Single.</p>
+        </div>
+        <p>
+          <Link to="/">
+            <span>Home </span>
+          </Link>
+          / Book Cover Deisgn
+        </p>
+      </div>
 
-export default BlogDetails
+      <div className="blog_details_section1">
+        <img src={propertySlide3} />
+        <p>
+          <strong>Author:</strong> Marouf Famous <strong>Category: </strong>
+          Travel
+          <strong> Date: </strong> 3 Apr. 2023
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogDetails;
