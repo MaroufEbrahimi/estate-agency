7fe7c9e3ef61084961ce88172dbc470de750cd69
Marouf <maroufibrahimi7264@gmail.com>
2023-04-03 21:42:47 -0700

    BlogDetails component created

---
+++ src/components/BlogDetails/BlogDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './BlogDetails.css'
+
+const BlogDetails = () => {
+  return (
+    <div className='blog_details'>BlogDetails</div>
+  )
+}
+
+export default BlogDetails
