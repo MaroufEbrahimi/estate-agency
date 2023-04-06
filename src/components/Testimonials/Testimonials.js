2c9629d09dca5a0a8b981a3b30179047df79e8a7
Marouf <maroufibrahimi7264@gmail.com>
2023-04-05 22:08:21 -0700

    using every object props

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -6,16 +6,19 @@ const Testimonials = (props) => {
   return (
     <div className="testimonials_details">
       <div className="testimonials_details_box">
-        <img src={props.img} />
+        <img src={props.testimonialsObj[1].img} />
       </div>
       <div className="testimonials_details_box">
-        <span class="ion-ios-quote">1</span>
+        <span className="ion-ios-quote">1</span>
         <div className="testimonials_text">
-          <p>{props.text}</p>
+          <p>{props.testimonialsObj[1].text}</p>
         </div>
         <div className="testimonials_profile">
-          <img src={props.miniImg} className="testimonials_name_img" />
-          <p>{props.name}</p>
+          <img
+            src={props.testimonialsObj[1].miniImg}
+            className="testimonials_name_img"
+          />
+          <p>{props.testimonialsObj[1].name}</p>
         </div>
       </div>
     </div>
