4e7f3485243cd2e855300d1f5e832e916ce1a12d
Marouf <maroufibrahimi7264@gmail.com>
2023-04-12 17:48:39 -0700

    create Loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Loading.css"
+
+function Loading() {
+  return (
+    <div className="spinner__wrapper">
+      <div className="spinner"></div>
+    </div>
+  )
+}
+
+export default Loading
