ea351cc10df29bf99c5f40a3cd8396578e6522ed
Marouf <maroufibrahimi7264@gmail.com>
2023-04-12 17:48:20 -0700

    added font-awesome

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import "./assets/fonts/font-awesome/css/font-awesome.min.css"
+import App from "./App"
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const container = document.getElementById("root")
+const root = createRoot(container)
 
-root.render(<App />);
+root.render(<App />)
