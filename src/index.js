af286ee207f84eb2f383fe9ddf1b5a64e56a6681
Marouf <maroufibrahimi7264@gmail.com>
2023-04-13 05:19:54 -0700

    import ionicons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import "./assets/fonts/font-awesome/css/font-awesome.min.css"
+import "./assets/fonts/ionicons/css/ionicons.min.css"
 import App from "./App"
 
 const container = document.getElementById("root")
