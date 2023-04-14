45b6bfd2a237a6bd40522b14fa73bb0bf53e3596
Marouf <maroufibrahimi7264@gmail.com>
2023-04-14 04:59:28 -0700

    added social media for every agent

--- src/components/Agent/Agent.js
+++ src/components/Agent/Agent.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from "react"
+import { Link } from "react-router-dom"
 
-import "./Agent.css";
+import "./Agent.css"
 
 const Agent = (props) => {
   return (
@@ -25,10 +25,30 @@ const Agent = (props) => {
               <span>Email:</span> {props.email}
             </p>
           </div>
+          <div className="agent_layer_socialmedia">
+            <a href="https://twitter.com/01Marouf" target="_blank">
+              <i className="fa fa-twitter"></i>
+            </a>
+            <a href="https://www.instagram.com/marouffmfm/" target="_blank">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/marouf-ebrahimi-7b6312237"
+              target="_blank"
+            >
+              <i className="fa fa-linkedin"></i>
+            </a>
+            <a href="https://www.pinterest.com/maroufebrahimi/" target="_blank">
+              <i className="fa fa-github"></i>
+            </a>
+            <a href="/" target="_blank">
+              <i className="fa fa-pinterest"></i>
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Agent;
+export default Agent
