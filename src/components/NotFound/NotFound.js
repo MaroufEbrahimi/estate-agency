6c3c5d62c55be4144e9451c5ecd619b0f12940db
Marouf <maroufibrahimi7264@gmail.com>
2023-04-15 18:08:29 -0700

    created NotFound component

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import "./NotFound.css"
+
+const NotFound = () => {
+  return (
+    <div className="notfound">
+      <h1>NotFound</h1>
+    </div>
+  )
+}
+
+export default NotFound
