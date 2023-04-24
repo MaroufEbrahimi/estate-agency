34b4f8f7292f283471c1cccd963a7bb50e1a2744
Marouf <maroufibrahimi7264@gmail.com>
2023-04-24 15:24:29 -0700

    added daynamic date

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -2,6 +2,50 @@ import React from "react"
 
 import "./CommentList.css"
 const CommentList = (props) => {
+  let day = new Date().getDate()
+  let month = new Date().getMonth()
+  let year = new Date().getFullYear()
+  let date
+
+  switch (month) {
+    case 0:
+      date = day + " Jan " + year
+      break
+    case 1:
+      date = day + " Feb " + year
+      break
+    case 2:
+      date = day + " Mar " + year
+      break
+    case 3:
+      date = day + " Apr " + year
+      break
+    case 4:
+      date = day + " May " + year
+      break
+    case 5:
+      date = day + " Jun " + year
+      break
+    case 6:
+      date = day + " Jul " + year
+      break
+    case 7:
+      date = day + " Aug " + year
+      break
+    case 8:
+      date = day + " Sep " + year
+      break
+    case 9:
+      date = day + " Oct " + year
+      break
+    case 10:
+      date = day + " Nov " + year
+      break
+    case 11:
+      date = day + " Dec " + year
+      break
+  }
+
   return (
     <div className="comments">
       <ul>
@@ -11,7 +55,7 @@ const CommentList = (props) => {
             <div>
               <h3>{item.name}</h3>
               <p>{item.email}</p>
-              <i>7 Apr 2023</i>
+              <i>{date}</i>
               <p>{item.comment}</p>
             </div>
           </li>
@@ -22,4 +66,3 @@ const CommentList = (props) => {
 }
 
 export default CommentList
-//   onClick={props.onRemoveItem.bind(this, item.id)}
